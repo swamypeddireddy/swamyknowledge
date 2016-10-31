a0d0829ede3e583ab051e308c8ee8ca4e2c3a08d
Vivek-Ausekar <vivek.ausekar@fiables.com>
2016-10-31 11:56:30 +0400

    Adding fixes for login functinality
    -- Added code to show logout option instead of register, if user is logged in to the system.

--- application/views/common/footer.php
+++ application/views/common/footer.php
@@ -1,3 +1,10 @@
+<script>
+    if ('' != "<?php echo $this->session->userdata('session_userId'); ?>" && "none" == $('#Logout').css('display')) {
+
+        $('#User_Register').css('display', 'none');
+        $('#Logout').css('display', 'block');
+    };
+</script>
 <footer>
     <div class="main">
         <div class="foot_div">
